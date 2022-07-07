293dcf1aa485176ccdbb743a4a996d954f1f9a10
niteshsharma9199 <niteshsharma9199@gmail.com>
2022-07-07 22:49:05 +0530

    Reversed of a String

---
+++ reversedString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ReversedString
+{
+    class Reversed
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter your name: ");
+            var names = Console.ReadLine();
+
+            //creating array
+            char[]? array = new char[names.Length];
+            for(var i= names.Length; i>0; i--)
+            {
+                array[names.Length - i] = names[i - 1];
+            }
+
+            var reversed = new string(array);
+            Console.WriteLine("Reversed name: " + reversed);
+
+        }
+    }
+}
